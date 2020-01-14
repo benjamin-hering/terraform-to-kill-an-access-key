b45c882d0bc7cfb3b77b7a511a5fb56c3e72eb15
benjamin-hering <benjamin.hering@gmail.com>
2020-01-13 22:01:04 -0800

    Update and rename modules/okta_api_user to modules/okta_api/iam.tf

---
+++ modules/okta_api/iam.tf
@@ -0,0 +1,8 @@
+resource "aws_iam_user" "okta_api" {
+  name = "okta-api"
+}
+
+resource "aws_iam_user_policy" "okta_api" {
+  user   = "${aws_iam_user.okta_api.name}"
+  policy = "${data.aws_iam_policy_document.okta_api_user_policy.json}"
+}
