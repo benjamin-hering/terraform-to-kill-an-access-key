04a4e6771bd00fa7c00662d7639bd202c62f0636
benjamin-hering <benjamin.hering@gmail.com>
2020-01-13 23:17:13 -0800

    Update variables.tf

--- modules/saml_iam_role/variables.tf
+++ modules/saml_iam_role/variables.tf
@@ -1,3 +1,8 @@
+variable "role_name" {
+  description = "Role Name"
+  default     = ""
+}
+
 variable "federated_principal_arn" {
   description = "SAML ARN from provider for principal trust"
   default     = ""
