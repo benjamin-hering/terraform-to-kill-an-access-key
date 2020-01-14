25cfd22a84721ccb8826c6f0617a0305ea655bf7
benjamin-hering <benjamin.hering@gmail.com>
2020-01-13 22:19:30 -0800

    Create role.tf

---
+++ modules/saml_iam_role/role.tf
@@ -0,0 +1,5 @@
+resource "aws_iam_role" "this" {
+  name                 = "${local.full_role_name}"
+  max_session_duration = "${local.max_session_duration}"
+  assume_role_policy   = "${data.aws_iam_policy_document.assume_role_saml.json}"
+}
