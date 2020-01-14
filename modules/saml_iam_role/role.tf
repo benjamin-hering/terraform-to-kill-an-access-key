6aa5c6d23dc28b342b5a270ecc01b5a812a42dd4
benjamin-hering <benjamin.hering@gmail.com>
2020-01-13 23:16:26 -0800

    Update role.tf

--- modules/saml_iam_role/role.tf
+++ modules/saml_iam_role/role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "this" {
   name                 = "${var.role_name}"
-  max_session_duration = "3600"
+  max_session_duration = "${local.max_session_duration}"
   assume_role_policy   = "${data.aws_iam_policy_document.assume_role_saml.json}"
 }
